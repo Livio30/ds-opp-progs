b80ff8b71d3fc73230af260bebdf18fa0c79fa96
Livio Fran Mascarenhas <47179761+Livio30@users.noreply.github.com>
2021-10-28 15:48:20 +0530

    Expt 1 q1-q9

---
+++ 3 Sem/OOP/expt15.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+	int num, i, fact = 1;
+	cout << "Enter the number to find it's Factorial: ";
+	cin >> num;
+	if (num == 0 || num == 1)
+		fact = 1;
+	else
+		for (i = 1;i <= num;i++)
+			fact *= i;
+	cout << "\nFactorial :" << fact;
+	_getch();
+}
---
+++ 3 Sem/OOP/expt17.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+	int r;
+	float area;
+	cout << "Enter the radius : ";
+	cin >> r;
+	area = 3.14 * r * r;
+	cout << "Area: " << area;
+	_getch();
+}
