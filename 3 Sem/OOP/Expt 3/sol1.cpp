d16d22875428e3aeafd41c31007896fcd09251e0
Livio Fran Mascarenhas <47179761+Livio30@users.noreply.github.com>
2021-11-12 12:44:11 +0530

    Expt 3

---
+++ 3 Sem/OOP/Expt 3/sol1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<conio.h>
+using namespace std;
+
+
+int square(int x) {
+	return x*x;
+}
+
+double square(double x) {
+	return x*x;
+}
+
+int main()
+{
+	int x; double y;
+
+	cout << "Enter the Int Number: ";
+	cin >> x;
+	cout << "Square of integer number is " << square(x) << endl;
+
+	cout << "Enter the double Number: ";
+	cin >> y;
+	cout << "Square of double number is " << square(y) << endl;
+	_getch();
+}
---
+++ 3 Sem/OOP/Expt 3/sol5.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int power(int base, int exponent) {
+	if (exponent == 1)
+		return base;
+	return base*power(base, exponent - 1);
+}
+
+int main() {
+	int base = 0, exponent = 0;
+
+	cout << "Enter the base value:\t";
+	cin >> base;
+	cout << "Enter the exponent:\t";
+	cin >> exponent;
+
+	if (exponent < 1) {
+		cout << "Please enter exponent greater or equal to 1";
+	}
+	else {
+		cout << "Result: " << power(base, exponent);
+	}
+
+	_getch();
+}
