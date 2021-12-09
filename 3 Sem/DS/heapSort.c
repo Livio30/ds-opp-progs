362b564f8e0932c061194f23993115c8207b5cd4
Livio Fran Mascarenhas <47179761+Livio30@users.noreply.github.com>
2021-12-09 07:29:26 +0530

    Change Program HeapSort

--- 3 Sem/DS/heapSort.c
+++ 3 Sem/DS/heapSort.c
@@ -1,50 +1,62 @@
-#include <stdio.h>  
-void heapify(int a[], int n, int i)  
-{  
-    int largest = i; 
-    int left = 2 * i + 1;
-    int right = 2 * i + 2; 
-    if (left < n && a[left] > a[largest])  
-        largest = left;  
-    if (right < n && a[right] > a[largest])  
-        largest = right;  
-    if (largest != i) {  
-        int temp = a[i];  
-        a[i] = a[largest];  
-        a[largest] = temp;  
-          
-        heapify(a, n, largest);  
-    }  
-}  
-void heapSort(int a[], int n)  
-{  
-    for (int i = n / 2 - 1; i >= 0; i--)  
-        heapify(a, n, i);   
-    for (int i = n - 1; i >= 0; i--) { 
-        int temp = a[0];  
-        a[0] = a[i];  
-        a[i] = temp;  
-          
-        heapify(a, i, 0);  
-    }  
-}    
-void printArr(int arr[], int n)  
-{  
-    for (int i = 0; i < n; ++i)  
-    {  
-        printf("%d", arr[i]);  
-        printf(" ");  
-    }  
-          
-}  
-int main()  
-{  
-    int a[] = {48, 10, 23, 43, 28, 26, 1};  
-    int n = sizeof(a) / sizeof(a[0]);  
-    printf("Before sorting array elements are - \n");  
-    printArr(a, n);  
-    heapSort(a, n);  
-    printf("\nAfter sorting array elements are - \n");    
-    printArr(a, n);  
-    return 0;  
-} 
+#include<stdio.h>
+
+void swap(int *a,int *b)
+{
+    int temp;
+    temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
+
+void maxHeapify(int  *arr,int i,int size)
+{
+    int largest=i,left,right;
+    left=(2*i)+1;
+    right=(2*i)+2;
+    if(left<size && arr[left]>arr[largest])
+        largest=left;
+    if(right<size && arr[right]>arr[largest])
+        largest=right;
+    if(largest!=i)
+    {
+        swap(&arr[largest],&arr[i]);
+        maxHeapify(arr,largest,size);
+    }
+}
+
+
+void createHeap(int *arr,int size)
+{
+    int i;
+
+    for(i=(size-2)/2; i>=0;i--)
+        maxHeapify(arr,i,size);
+}
+
+void heapSort(int *arr,int size)
+{
+    createHeap(arr,size);
+    while(size>1)
+    {
+        swap(&arr[0],&arr[size-1]);
+        --size;
+        maxHeapify(arr,0,size);
+    }
+}
+
+
+int main()
+{
+    int i,arr[100],size;
+    printf("Enter number of elements in an array:");
+    scanf("%d",&size);
+    printf("Enter %d elements:",size);
+    for(i=0;i<size;i++)
+        scanf("%d",&arr[i]);
+    heapSort(arr,size);
+    printf("Asscending order:");
+    for(i=0;i<size;i++)
+        printf("%d ",arr[i]);
+    return 0;
+}
