d0998e297fb02b68d5ca2a25791acb2df3dcdb35
Livio Fran Mascarenhas <47179761+Livio30@users.noreply.github.com>
2021-12-10 14:29:17 +0530

    Expt 7 Programs

---
+++ 3 Sem/OOP/Expt 7/sol74.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	int n;
+	cout << "Enter a decimal number" << endl;
+	cin >> n;
+	cout << n << " in HexaDecimal is: " << hex << n << endl;
+	cout << dec << n << " in Octal is: " << oct << n << endl;
+	cout << setbase(10) << n << " in decimal is: " << n << endl;
+	_getch();
+}
---
+++ 3 Sem/OOP/Expt 7/sol78.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int integerValue;
+
+	cout << " Before a bad input operation:"
+		<< "\n cin.rdstate(): " << cin.rdstate()
+		<< "\n cin.eof(): " << cin.eof()
+		<< "\n cin.fail(): " << cin.fail()
+		<< "\n cin.bad(): " << cin.bad()
+		<< "\n cin.good(): " << cin.good()
+		<< "\n\n Expects an integer, but enter a character: ";
+	cin >> integerValue; 
+	cout << endl;
+
+	cout << " After a bad input operation:"
+		<< "\n cin.rdstate(): " << cin.rdstate()
+		<< "\n cin.eof(): " << cin.eof()
+		<< "\n cin.fail(): " << cin.fail()
+		<< "\n cin.bad(): " << cin.bad()
+		<< "\n cin.good(): " << cin.good() << endl << endl;
+
+	cin.clear();
+	cout << " After cin.clear()" << "\n cin.fail(): " << cin.fail() << "\n cin.good(): " << cin.good() << endl;
+
+	_getch();
+}
